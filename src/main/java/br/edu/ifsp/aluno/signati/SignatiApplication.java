180c309597baba3763647c391a4baee725dcb621
Ananda Albrecht <ananda.albrecht@aluno.ifsp.edu.br>
2020-12-17 18:51:25 -0300

    Initial Commit: Basic structure and database config

---
+++ src/main/java/br/edu/ifsp/aluno/signati/SignatiApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.aluno.signati;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignatiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignatiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifsp/aluno/signati/SignatiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.aluno.signati;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignatiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
