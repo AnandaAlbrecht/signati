03a31415c6ed3699a743a4158513653bcaa38150
Victor <j7fernandes@gmail.com>
2021-01-06 19:42:52 -0300

    Change thymeleaf import

--- src/main/java/br/edu/ifsp/aluno/signati/controllers/HelloController.java
+++ src/main/java/br/edu/ifsp/aluno/signati/controllers/HelloController.java
@@ -1,16 +1,25 @@
 package br.edu.ifsp.aluno.signati.controllers;
 
+import br.edu.ifsp.aluno.signati.services.PetitionService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/")
+@RequiredArgsConstructor
 public class HelloController {
 
-    @GetMapping("/hello")
-    public ModelAndView hello(){
-        ModelAndView mv = new ModelAndView("hello");
-        mv.addObject("name","User");
+    private final PetitionService petitionService;
+
+    @GetMapping
+    public ModelAndView helloController(){
+
+        var mv = new ModelAndView("home");
+        mv.addObject("petitions", petitionService.getAllPetitionsByQuery(null));
+
         return mv;
     }
 }
